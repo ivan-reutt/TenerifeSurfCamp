7f64486e609fb453b42552e6680217ad10219a1f
JohnReut <kotopesbeatboxer@gmail.com>
2023-12-18 00:17:40 +0000

    Add google tag manager

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -29,6 +29,23 @@ const config: GatsbyConfig = {
             },
         },
         {
+            resolve: "gatsby-plugin-google-tagmanager",
+            options: {
+                id: "GTM-TWT3PWW6'",
+
+                // Include GTM in development.
+                //
+                // Defaults to false meaning GTM will only be loaded in production.
+                includeInDevelopment: false,
+
+                // datalayer to be set before GTM is loaded
+                // should be an object or a function that is executed in the browser
+                //
+                // Defaults to null
+                defaultDataLayer: { platform: "gatsby" },
+            },
+        },
+        {
             resolve: `gatsby-source-filesystem`,
             options: {
                 path: `${__dirname}/locales`,
