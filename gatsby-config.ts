1abe8f97ebb5532d0763bb0779068807027aff02
JohnReut <kotopesbeatboxer@gmail.com>
2026-01-11 22:25:49 +0300

    Add canonical

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -95,6 +95,12 @@ const config: GatsbyConfig = {
                 pixelId: "642630721362046",
             },
         },
+        {
+            resolve: `gatsby-plugin-canonical-urls`,
+            options: {
+                siteUrl: `https://localclub.me/`,
+            },
+        },
     ],
 };
 
